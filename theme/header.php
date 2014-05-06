8c25415735d4263e3d33b44b97bdf9fc7924708d
aaronmadethis <aaron@aaronmadethis.com>
2014-05-05 21:30:22 -0400

    IE 10 Conditional

--- theme/header.php
+++ theme/header.php
@@ -27,6 +27,9 @@
 <!-- Typekit Fonts -->
 <script type="text/javascript" src="//use.typekit.net/les2ygm.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<!--[if lt IE 10]>
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/css/style_ie.css" />
+<![endif]-->
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
